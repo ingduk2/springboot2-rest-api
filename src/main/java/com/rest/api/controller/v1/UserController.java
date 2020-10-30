d0e81518e6e9fd0e72a17821ec375767deb4b0d1
ingduk2 <ingduk2@naver.com>
2020-10-31 03:39:54 +0900

    springboot2 rest api(3) - h2 database

---
+++ src/main/java/com/rest/api/controller/v1/UserController.java
@@ -0,0 +1,33 @@
+package com.rest.api.controller.v1;
+
+import com.rest.api.entity.User;
+import com.rest.api.repo.UserJpaRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "v1")
+public class UserController {
+    private final UserJpaRepo userJpaRepo;
+
+    @GetMapping(value = "/user")
+    public List<User> findAllUSer() {
+        return userJpaRepo.findAll();
+    }
+
+    @PostMapping(value = "/user")
+    public User save() {
+        User user = User.builder()
+                .uid("ss@naver.com")
+                .name("s1`s1s1s1s")
+                .build();
+        return userJpaRepo.save(user);
+    }
+
+}
